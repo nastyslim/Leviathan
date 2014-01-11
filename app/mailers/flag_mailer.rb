4f5135fe73c37b3be1cdf6d69a8a1eedb30d77e0
nastyslim <eduard.y.popov@gmail.com>
2014-01-11 00:36:03 -0800

    email2

--- app/mailers/flag_mailer.rb
+++ app/mailers/flag_mailer.rb
@@ -2,7 +2,8 @@ class FlagMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def flag_email
+  	@steppy = Steppy
   	@admin = "eduard.y.popov@gmail.com"
-    mail(to: @admin, subject: 'Welcome to My Awesome Site')
+    mail(to: @admin, subject: 'Welcome to My Awesome Site #{@steppy.goal}"')
   end
 end
